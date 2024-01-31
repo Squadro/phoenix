ce40e71bb6cbf5c04b576b7051467dbefb4b6814
Ravi Teja Muppalla <153738634+bonitoravi@users.noreply.github.com>
2024-01-31 04:06:54 -0600

    Added the Multiple Kafka Consumers

--- embedding_generator/database/repository.py
+++ embedding_generator/database/repository.py
@@ -11,16 +11,7 @@ from embedding_generator.model.product_variant_information import (
 logger = logging.getLogger(__name__)
 
 
-class SingletonMeta(type):
-    _instances = {}
-
-    def __call__(cls, *args, **kwargs):
-        if cls not in cls._instances:
-            cls._instances[cls] = super(SingletonMeta, cls).__call__(*args, **kwargs)
-        return cls._instances[cls]
-
-
-class EmbeddingRepository(metaclass=SingletonMeta):
+class EmbeddingRepository:
     @staticmethod
     def save_embedding(data, embedding):
         try:
--- message_consumer/management/commands/consume_message.py
+++ message_consumer/management/commands/consume_message.py
@@ -1,32 +1,30 @@
 from django.core.management import BaseCommand
 
 from constant import KAFKA_BOOTSTRAP_SERVERS, KAFKA_GROUP_ID, KAFKA_MIGRATION_TOPIC
-from message_consumer.callbacks import custom_callback
 from message_consumer.kafka_consumer import KafkaConsumer
 
 
 class Command(BaseCommand):
-    help = 'Consume messages from Kafka topic'
+    help = "Consume messages from Kafka topic"
 
     def handle(self, *args, **options):
-        # Kafka configuration
+        consumers = []
+        no_of_consumers = 5
         kafka_bootstrap_servers = KAFKA_BOOTSTRAP_SERVERS
         kafka_group_id = KAFKA_GROUP_ID
         kafka_topics = KAFKA_MIGRATION_TOPIC
-
-        # Start Kafka consumer in a separate thread
-        kafka_consumer_thread = KafkaConsumer(
-            bootstrap_servers=kafka_bootstrap_servers,
-            group_id=kafka_group_id,
-            topics=kafka_topics
-        )
-        kafka_consumer_thread.process_message = custom_callback
-        kafka_consumer_thread.start()
-
+        for partition in range(no_of_consumers):
+            consumer = KafkaConsumer(
+                kafka_bootstrap_servers, kafka_group_id, kafka_topics
+            )
+            consumer.start()
+            consumers.append(consumer)
         try:
             # Keep the main thread alive to handle interrupts
-            kafka_consumer_thread.join()
+            for consumer in consumers:
+                consumer.join()
         except KeyboardInterrupt:
-            # Stop the Kafka consumer thread if interrupted
-            kafka_consumer_thread.stop()
-            kafka_consumer_thread.join()
+            # Stop the Kafka consumer threads if interrupted
+            for consumer in consumers:
+                consumer.stop()
+                consumer.join()
